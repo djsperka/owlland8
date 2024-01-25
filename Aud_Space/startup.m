a8282544058af256cc3dae152ab495b4767957cb
Daniel Sperka <djsperka@ucdavis.edu>
2024-01-25 12:01:51 -0800

    Fix foldernames with fullfile instead of strcat

--- Aud_Space/startup.m
+++ Aud_Space/startup.m
@@ -1,10 +1,8 @@
 function startup
 
 fprintf('Welcome to OwlLand ... loading default paths\nEdit default paths by adjust startup.m in your startup folder\n')
-currentdir=cd;
-ASPath=strcat(currentdir,'\Aud_space_files');
-addpath (ASPath);
-addpath (strcat(currentdir,'\Aud_space_files\Flexible_Stim'))
-%addpath (strcat(currentdir,'\..\DataAnalysis'))
-addpath (strcat(currentdir,'\..\Equalization'))
+addpath (fullfile(cd, 'Aud_space_files'));
+addpath (fullfile(cd, 'Aud_space_files', 'Flexible_Stim'))
+addpath(fullfile(cd, '..', 'Equalization'));
+%addpath(fullfile(cd, '..', 'DataAnalysis'));
 
