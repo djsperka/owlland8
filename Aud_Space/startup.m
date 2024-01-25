3b521a3ce8aabfa846ffa6ed87a5cfc791d77f54
Daniel Sperka <djsperka@ucdavis.edu>
2024-01-25 14:33:42 -0800

    cleanup. DataAnalysis folder is ???

--- Aud_Space/startup.m
+++ Aud_Space/startup.m
@@ -1,8 +1,9 @@
 function startup
 
 fprintf('Welcome to OwlLand ... loading default paths\nEdit default paths by adjust startup.m in your startup folder\n')
-addpath (fullfile(cd, 'Aud_space_files'));
-addpath (fullfile(cd, 'Aud_space_files', 'Flexible_Stim'))
+addpath(cd);
+addpath(fullfile(cd, 'Aud_space_files'));
+addpath(fullfile(cd, 'Aud_space_files', 'Flexible_Stim'))
 addpath(fullfile(cd, '..', 'Equalization'));
 %addpath(fullfile(cd, '..', 'DataAnalysis'));
 
