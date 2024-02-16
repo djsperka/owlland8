b552603b8e45e631d9e907da5ad173c79a3ac882
Daniel Sperka <djsperka@ucdavis.edu>
2024-02-15 16:14:23 -0800

    Buffer created must be a row vector, not a column vector (row vec required by WriteTagV)

--- Aud_Space/Aud_space_files/makeNoiseTone.m
+++ Aud_Space/Aud_space_files/makeNoiseTone.m
@@ -1,4 +1,10 @@
 function [nSampleNoise, nSampleTone, sound] = makeNoiseTone(fs, amplitude, dur1, freqlo1, freqhi1, dur2, freq2)
+%makeNoiseTone Creates a single buffer containing two sound stimuli - noise
+%and a tone. Returns a single row vector.
+%   The first stim is band filtered noise, the second is a pure tone. There
+%   is no separation between the stimuli in the returned buffer. The first
+%   two return values are the number of samples of each stim in the buffer.
+    
 
     % Compute number of samples for each stim. 
     % I put an extra 100ms samples in each in case of edge effects in the TDT
@@ -9,7 +15,7 @@ function [nSampleNoise, nSampleTone, sound] = makeNoiseTone(fs, amplitude, dur1,
     dur2Seconds = 0.001 * (dur2 + 100);
     t=[0:(1/fs):dur2Seconds-(1/fs)];  % # points to use
     nSampleTone = length(t);
-    sound = zeros(nSampleNoise + nSampleTone, 1);
+    sound = zeros(1, nSampleNoise + nSampleTone);
  
     %% Noise
     y=amplitude*randn(nSampleNoise + 10000, 1);
